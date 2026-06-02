5b657e6f020bb2ae2f33c342588bb7b44b8aa980
Victor Hugo Andrade <127215018+vhandradee@users.noreply.github.com>
2026-06-01 23:33:44 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as str
+
+st.title("DataBurn - Monitoramento Espacial")
+st.subheader("Equipe: Engenharia de Software - FIAP")
+st.markdown("---")
+st.write("**Propósito:** Processamento de dados de sensoriamento remoto para mitigação de incêndios florestais.")
+st.write("**ODS Conectado:** ODS 13 - Ação Contra a Mudança Global do Clima.")
